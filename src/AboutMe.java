5558616c9bfe57fa8e2f73955fa42a9ac9d75548
megofast <33850107+megofast@users.noreply.github.com>
2018-01-21 21:58:16 -0600

    Initial commit

---
+++ src/AboutMe.java
@@ -0,0 +1,8 @@
+public class AboutMe {
+    public static void main(String[] args) {
+        System.out.println("My name is Dave.");
+        System.out.println("I am 29 years old.");
+        System.out.println("I have been programming since I was 12.");
+        System.out.println("The first programming language I learned was Basic");
+    }
+}
